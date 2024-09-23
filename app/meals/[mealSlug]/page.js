803042281ff68759abe4809cad283d69e0112fcf
Stephen Moore <judo2000@comcast.net>
2024-09-23 14:32:17 -0600

    Initial commit.  Create project and add basic page and header component

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -3,6 +3,20 @@ import { getMeal } from "@/lib/meals";
 import classes from "./page.module.css";
 import { notFound } from "next/navigation";
 
+// Generate metadata for a dynamic page.
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 export default async function MealDetailsPage({ params }) {
   const meal = await getMeal(params.mealSlug);
 
